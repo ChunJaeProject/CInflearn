dcfb65c74b77b7c3b47ca287406d7213c147a0f6
jongyun <cjy205205@naver.com>
2024-03-25 16:05:52 +0900

    bug

--- src/main/java/controller/LikeServlet.java
+++ src/main/java/controller/LikeServlet.java
@@ -18,7 +18,7 @@ public class LikeServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HttpSession session = req.getSession();
+		
 		
 		
 		int like = Integer.parseInt(req.getParameter("like"));
